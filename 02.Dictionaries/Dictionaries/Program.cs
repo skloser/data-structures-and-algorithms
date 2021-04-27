28b09f268c0416d519e9032d29ba74ddd13a3f87
Крум Багашев <krum.bagashev@gmail.com>
2021-04-27 14:42:01 +0300

    Added a firsr recurring character question

---
+++ 02.Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstRecurringCharacter(new int[4] { 2, 3, 4, 5 }));
+        }
+
+        /*
+     
+            Google question: 
+            
+            Return the first recurring characted in array
+        
+            Given an array: [2,5,1,2,3,5,1,2,4]
+            It should return 2
+
+            Give an array: [2,1,1,2,3,5,1,2,4]
+            It should return 1
+
+            Give an array: [2,3,4,5]
+            it should return undefined
+     
+            
+        */
+
+        static int FirstRecurringCharacter(int[] array)
+        {
+            var set = new HashSet<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (set.Contains(array[i]))
+                {
+                    return array[i];
+                }
+
+                set.Add(array[i]);
+            }
+
+            throw new ArgumentException("Array does not contain reccuring characters");
+        }
+    }
+
+    
+}
